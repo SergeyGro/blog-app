ba0ee74f1c8be71856cfff9a44b3d937e90f725a
Sergey <sirgroshev@yandex.ru>
2026-04-28 18:07:31 +0300

    Реализовал функционал пагинации

--- js/render.js
+++ js/render.js
@@ -4,46 +4,30 @@ import { getPosts } from "./api.js";
 
 let allPosts = [];
 let currentPage = 1;
-const pageСounts = 6; 
+const pageСounts = 8; 
 
 export async function initPosts() {
     allPosts = await getPosts();
     renderPosts();
     renderPagination();
-}
-
-function renderPosts(){
-    const postsBlock = document.querySelector('.posts-block');
-    postsBlock.innerHTML = `${allPosts.map(post => `
-            <article class="post">
-                <h2>${post.title}</h2>
-                <p>${post.body}</p>
-                <a href="/get/${post.postId}" class="read-more">Читать далее</a>
-            </article>
-        `).join('')}
-    `
+    handlePagesNav();
 }
 
 export function renderHome() {
     return `
         <div class="container">
-            <h1>Посты</h1>
-            <div class="posts-block">
+            <div class="posts">
+                <h1>Посты</h1>
+                <div class="posts-block">
 
+                </div>
             </div>
-            <nav class="pagination"></nav>
+            <nav class="pagination">
+                <button id="prev-page">Назад</button>
+                <ul class="nav-pages"></ul>
+                <button id="next-page">Вперед</button>
+            </nav>
         </div>`;
-}
-
-function renderPagination() {
-    const nav = document.querySelector('.pagination');
-    const maxPages = Math.ceil(allPosts.length / pageСounts);
-    let pageElements = '';
-    for (let i = 1; i <= maxPages; i++){
-        const page = `<a href="#">${i}</a>`;
-        pageElements = pageElements + page;
-    }
-    nav.innerHTML = pageElements;
 }
 
 export function renderPost() {
@@ -53,4 +37,62 @@ export function renderPost() {
             <p>Описание поста</p>
             <p>Еще инфа</p>
         </div>`;
+}
+
+function renderPosts(){
+    const postsBlock = document.querySelector('.posts-block');
+    const end = pageСounts * currentPage;
+    const start = end - pageСounts;
+    const portionPosts = allPosts.slice(start, end);
+    postsBlock.innerHTML = `${portionPosts.map(post => `
+            <article class="post">
+                <h2>${post.title}</h2>
+                <p>${post.body}</p>
+                <a href="/get/${post.postId}" class="read-more">Читать далее</a>
+            </article>
+        `).join('')}
+    `
+}
+
+function renderPagination() {
+    const nav = document.querySelector('.pagination');
+    const maxPages = Math.ceil(allPosts.length / pageСounts);
+    let pageElements = '';
+    for (let i = 1; i <= maxPages; i++){
+        const page = `<li><a href="#">${i}</a></li>`;
+        pageElements = pageElements + page;
+    }
+    nav.childNodes[3].innerHTML = pageElements;
+    showCurrentPage();
+}
+
+function handlePagesNav() {
+    const pages = document.querySelector('.nav-pages');
+    const prevBtn = document.getElementById('prev-page');
+    const nextBtn = document.getElementById('next-page');
+    pages.childNodes.forEach(elem => elem.firstChild.addEventListener('click', e => {
+        e.preventDefault();
+        currentPage = Number(e.target.text);
+        showCurrentPage()
+        renderPosts();
+    }));
+    prevBtn.addEventListener('click', () => {
+        if(currentPage > 1) {
+            currentPage = currentPage - 1;
+            showCurrentPage()
+            renderPosts();
+        }
+    })
+    nextBtn.addEventListener('click', () => {
+        if(currentPage < Math.ceil(allPosts.length / pageСounts)) {
+            currentPage = currentPage + 1;
+            showCurrentPage()
+            renderPosts();
+        }
+    })
+}
+
+function showCurrentPage() {
+    const pages = document.querySelector('.nav-pages');
+    pages.childNodes.forEach(elem => elem.firstChild.classList.toggle('active-page', Number(elem.firstChild.text) === currentPage));
 }
